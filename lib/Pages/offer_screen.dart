0444e549f096c8913934a66f5a84282a475acfee
Elif Özcan <ozcann159@gmail.com>
2024-05-22 22:00:53 +0300

    offer_screen edit

--- lib/Pages/offer_screen.dart
+++ lib/Pages/offer_screen.dart
@@ -1,11 +1,10 @@
-// offer_screen.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loadspotter/blocs/offer/offer_bloc.dart';
 import 'package:loadspotter/blocs/offer/offer_event.dart';
 import 'package:loadspotter/blocs/offer/offer_state.dart';
 import 'package:loadspotter/models/offer.dart';
+import 'package:loadspotter/repositories/firestore_services.dart';
 
 
 class OfferScreen extends StatelessWidget {
@@ -15,41 +14,84 @@ class OfferScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => OffersBloc(firestoreService: FirestoreService()), 
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Teklifler'),
+        ),
+        body: BlocBuilder<OffersBloc, OffersState>(
+          builder: (context, state) {
+            if (state is OffersLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is OffersLoaded) {
+              final offers = state.offers;
+
+              return ListView.builder(
+                itemCount: offers.length,
+                itemBuilder: (context, index) {
+                  final offer = offers[index];
+                  return ListTile(
+                    title: Text('Teklif: ${offer.price} TL'),
+                    subtitle: Text('Durum: ${offer.status}'),
+                  );
+                },
+              );
+            } else {
+              return Center(child: Text('Teklifler yüklenemedi'));
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Yeni bir teklif ekleme işlemi başlat
+            _navigateToAddOfferScreen(context);
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+
+  void _navigateToAddOfferScreen(BuildContext context) {
+    // Yeni teklif ekranına yönlendirme
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => AddOfferScreen(loadPostingId: loadPostingId),
+      ),
+    );
+  }
+}
+
+class AddOfferScreen extends StatelessWidget {
+  final String loadPostingId;
+
+  AddOfferScreen({required this.loadPostingId});
+
+  @override
+  Widget build(BuildContext context) {
+    // Yeni teklif ekranının oluşturulması
     return Scaffold(
       appBar: AppBar(
-        title: Text('Teklifler'),
+        title: Text('Yeni Teklif Ekle'),
       ),
-      body: BlocBuilder<OffersBloc, OffersState>(
-        builder: (context, state) {
-          if (state is OffersLoading) {
-            return Center(child: CircularProgressIndicator());
-          } else if (state is OffersLoaded) {
-            final offers = state.offers;
-
-            return ListView.builder(
-              itemCount: offers.length,
-              itemBuilder: (context, index) {
-                final offer = offers[index];
-                return ListTile(
-                  title: Text('Teklif: ${offer.price} TL'),
-                  subtitle: Text('Durum: ${offer.status}'),
-                );
-              },
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Yeni teklif ekleme işlemi
+            final newOffer = Offer(
+              id: 'new_offer_id',
+              userId: 'user123',
+              loadPostingId: loadPostingId,
+              price: 0, // Burada varsayılan bir fiyat belirleyebilirsiniz
+              status: 'Pending', // Varsayılan durumu belirleyin
+              // Diğer özellikler
             );
-          } else if (state is OfferError) {
-            return Center(child: Text('Hata: ${state.message}'));
-          } else {
-            return Center(child: Text('Teklifler yüklenemedi'));
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Teklif ekleme işlemini başlat
-          final newOffer = Offer(price: 0, status: '', id: '', loadPostingId: '', userId: '');
-          BlocProvider.of<OffersBloc>(context).add(OfferAdded(newOffer));
-        },
-        child: Icon(Icons.add),
+            context.read<OffersBloc>().add(OfferAdded(newOffer));
+            Navigator.of(context).pop(); // Ekranı kapat
+          },
+          child: Text('Teklif Ekle'),
+        ),
       ),
     );
   }
