aa0390e2093625936464473bed3f89c01f835634
Elif Özcan <ozcann159@gmail.com>
2024-06-07 13:11:27 +0300

    cargo adversiter page add

--- lib/Pages/cargo_advertiser.dart
+++ lib/Pages/cargo_advertiser.dart
@@ -1,13 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loadspotter/Pages/login_page.dart';
 import 'package:loadspotter/Pages/profile.page.dart';
-import 'package:loadspotter/repositories/auth_repository.dart';
 
 class CargoAdvertiser extends StatelessWidget {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +15,16 @@ class CargoAdvertiser extends StatelessWidget {
       appBar: AppBar(
         actions: [
           IconButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ProfilePage()));
-              },
-              icon: Icon(
-                CupertinoIcons.person_circle,
-              )),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+            },
+            icon: Icon(
+              CupertinoIcons.person_circle,
+            ),
+          ),
           IconButton(
             icon: Icon(Icons.menu),
             onPressed: () {
@@ -31,42 +33,144 @@ class CargoAdvertiser extends StatelessWidget {
           ),
         ],
       ),
-      endDrawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.person_circle,
-              ),
-              title: Text('Profilim'),
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ProfilePage()));
-              },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/photo.png',
+              fit: BoxFit.cover,
             ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.chat_bubble_2_fill,
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.circular(10),
               ),
-              title: Text('Yardım'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                CupertinoIcons.power,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Platformda Anlık Durum',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Icon(Icons.info),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      const Icon(Icons.directions_car),
+                      const SizedBox(width: 5),
+                      StreamBuilder<QuerySnapshot>(
+                        stream: FirebaseFirestore.instance
+                            .collection('drivers')
+                            .snapshots(),
+                        builder: (context, driverSnapshot) {
+                          if (driverSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          }
+                          final activeCarriers =
+                              driverSnapshot.data!.docs.length;
+                          return Text(
+                            'Aktif Nakliyeciler: $activeCarriers',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      const Icon(Icons.assignment),
+                      const SizedBox(width: 5),
+                      StreamBuilder<QuerySnapshot>(
+                        stream: FirebaseFirestore.instance
+                            .collection('loadPostings')
+                            .snapshots(),
+                        builder: (context, postingSnapshot) {
+                          if (postingSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          }
+                          final activePostings =
+                              postingSnapshot.data!.docs.length;
+                          return Text(
+                            'Aktif İlanlar: $activePostings',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Toplam kullanıcı sayısı: 1000', // Burada kullanıcı sayısını belirliyor olabilirsiniz.
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-              title: Text('Çıkış'),
-              onTap: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()));
-              },
             ),
           ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(
+                  CupertinoIcons.person_circle,
+                ),
+                title: const Text('Profilim'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()),
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  CupertinoIcons.chat_bubble_2_fill,
+                ),
+                title: Text('Yardım'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  CupertinoIcons.power,
+                ),
+                title: const Text('Çıkış'),
+                onTap: () async {
+                  await FirebaseAuth.instance.signOut();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
